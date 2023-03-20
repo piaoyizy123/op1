86f84ada8a118e1c25991d4ff89d8c5c3e2a4cc7
piaoyizy123 <50651117+piaoyizy123@users.noreply.github.com>
2023-03-20 09:47:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,42 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+
+
+# 添加SSR-Plus
+rm -rf package/luci-app-ssr-plus
+git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
+
+
+
+
+# 添加PassWall
+rm -rf package/passwall
+rm -rf package/luci-app-passwall
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+git clone  https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+mv -f package/passwall package/luci-app-passwall
+
+# Add luci-app-openclash
+git clone -b master https://github.com/vernesong/OpenClash.git package-temp
+rm -rf package/lean/luci-app-openclash
+mv -f package-temp/luci-app-openclash package/lean/
+cat >> .config <<EOF
+CONFIG_PACKAGE_luci-app-openclash=y
+CONFIG_LIBCURL_COOKIES=y
+CONFIG_LIBCURL_FILE=y
+CONFIG_LIBCURL_FTP=y
+CONFIG_LIBCURL_HTTP=y
+CONFIG_LIBCURL_NO_SMB="!"
+CONFIG_LIBCURL_OPENSSL=y
+CONFIG_LIBCURL_PROXY=y
+CONFIG_PACKAGE_bash=y
+CONFIG_PACKAGE_ca-bundle=y
+CONFIG_PACKAGE_coreutils-nohup=y
+CONFIG_PACKAGE_curl=y
+CONFIG_PACKAGE_libcurl=y
+CONFIG_PACKAGE_libncurses=y
+CONFIG_PACKAGE_libreadline=y
+CONFIG_PACKAGE_terminfo=y
+EOF
